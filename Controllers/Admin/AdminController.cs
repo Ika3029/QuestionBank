c492d7f65828aa56c1ecc1c0bd49287b18860928
Ika3029 <133955073+Ika3029@users.noreply.github.com>
2025-11-01 21:27:06 +0800

    版面修改

---
+++ Controllers/Admin/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 專題MVC修正.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+       
+            // GET: /Admin/
+            public ActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
