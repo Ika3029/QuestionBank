c034fa6c42c7b67c7cf2fba55786edf0fc904f63
Ika3029 <133955073+Ika3029@users.noreply.github.com>
2025-12-26 20:34:10 +0800

    註釋

--- Models/DTOs/MQB.cs
+++ Models/DTOs/MQB.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity; // 為了 DbContext.Set<T>()
-using 專題MVC修正.Models; // 為了 MQBEntities
-// 命名空間請用你的實際命名空間
+using System.Data.Entity; 
+using 專題MVC修正.Models; 
+
 namespace 專題MVC修正.Models.DTOs
 {
     
-        // 只是查詢投影用，不是資料表，不要加 DbSet
+        
      public class ExamDselectlist
      {
             public int MQBClassPK { get; set; }
@@ -31,9 +31,9 @@ namespace 專題MVC修正.Models.DTOs
 
     public class ExamDetailRowVM
     {
-        public int ExamDetPK { get; set; }   // PK_ExamDetail
-        public double? ExamDefaultScore { get; set; } // float -> C# double?
-        public int? ExamMQBPK { get; set; }   // 可為 null
+        public int ExamDetPK { get; set; }   
+        public double? ExamDefaultScore { get; set; } 
+        public int? ExamMQBPK { get; set; }   
         public string QContent { get; set; }
         public string QAns { get; set; }
     }
